d91b5d55a25e90592e920ab59717e30dfbfcc5a6
Parabeac-Nest <admin@parabeac.com>
2022-10-28 16:57:03 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/iristest_text_styles.g.dart
@@ -0,0 +1,211 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class IristestTextStyles {
+/// This style appears in RWD article content card headlines
+static const TextStyle headline1 = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Avenir Next LT Pro',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle body1 = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Avenir Next LT Pro',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// Mobile
+static const TextStyle smallLabel2 = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.5,
+  fontFamily: 'Avenir Next LT Pro',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headline4 = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Avenir Next LT Pro',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// Mobile
+static const TextStyle label2Small = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.5,
+  fontFamily: 'Avenir Next LT Pro',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// This style appears in RWD landing page titles
+static const TextStyle display1 = TextStyle(
+  fontSize: 48,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Avenir Next LT Pro',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// Mobile
+static const TextStyle label1Small = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0.5,
+  fontFamily: 'Avenir Next LT Pro',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// This style appears in RWD video card titles
+static const TextStyle headline2 = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Avenir Next LT Pro',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headline5 = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Avenir Next LT Pro',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// Global medium wt list
+static const TextStyle list1 = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0,
+  fontFamily: 'Avenir Next LT Pro',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// Global smaller body copy
+
+static const TextStyle body2 = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Avenir Next LT Pro',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headline6 = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Avenir Next LT Pro',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// Mobile
+static const TextStyle link2Small = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.5,
+  fontFamily: 'Avenir Next LT Pro',
+  decoration: TextDecoration.underline,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle display2 = TextStyle(
+  fontSize: 32,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Avenir Next LT Pro',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle label2 = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.5,
+  fontFamily: 'Avenir Next LT Pro',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// This style appears in RWD page headlines
+static const TextStyle display3 = TextStyle(
+  fontSize: 32,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Avenir Next LT Pro',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// This style appears in large content card body copy
+static const TextStyle headline3 = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Avenir Next LT Pro',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle link2Large = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.5,
+  fontFamily: 'Avenir Next LT Pro',
+  decoration: TextDecoration.underline,
+  fontStyle: FontStyle.normal,
+)
+;
+/// Desktop
+static const TextStyle label1Large = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0.5,
+  fontFamily: 'Avenir Next LT Pro',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+/// Desktop
+static const TextStyle label2Large = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.5,
+  fontFamily: 'Avenir Next LT Pro',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}

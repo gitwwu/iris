2a8c913e1240d9d201d2d9f774fb8e0acfa35973
Parabeac-Nest <admin@parabeac.com>
2022-10-28 16:39:36 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/iristest_colors.g.dart
@@ -0,0 +1,62 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class IristestColors {
+/// Used for primary buttons and pressed states
+static const Color blue1 = Color(0xff012d70);
+
+static const Color fillWhite = Color(0xffffffff);
+/// Used as inactive state
+static const Color gray2 = Color(0xff878798);
+
+static const Color offWhite = Color(0xfff7f7f7);
+/// Used as primary text color.
+static const Color black = Color(0xff100f32);
+/// Used for both text and backgrounds.
+static const Color white = Color(0xffffffff);
+/// Used for any negative messaging
+static const Color red = Color(0xffb6006a);
+/// Used for links, etc.
+static const Color blue2 = Color(0xff0069b6);
+/// Used for secondary text in running copy.
+static const Color gray1 = Color(0xff585770);
+/// Used for any positive messaging
+static const Color green = Color(0xff006855);
+/// Borders, outlines. etc.
+static const Color gray3 = Color(0xffccd5df);
+/// Used for infographics, progress bars, etc.
+static const Color blue3 = Color(0xff72d4ff);
+/// Used for both brand and warning moments.
+static const Color yellow = Color(0xfffdd73c);
+/// Used for navigation backgrounds
+static const Color blue4 = Color(0xffe3f6ff);
+/// Used for primary inactive states
+static const Color lightYellow = Color(0xfffceeb3);
+
+static const Color distressScale1 = Color(0xffe4f1fa);
+
+static const Color distressScale2 = Color(0xffd4e9f9);
+
+static const Color distressScale3 = Color(0xffcddbf1);
+
+static const Color distressScale4 = Color(0xffc7c8e7);
+
+static const Color distressScale5 = Color(0xffc2b3da);
+
+static const Color distressScale6 = Color(0xffbf9bcc);
+
+static const Color distressScale7 = Color(0xffbb80ba);
+
+static const Color distressScale8 = Color(0xffb868ad);
+
+static const Color distressScale9 = Color(0xffb94496);
+
+static const Color distressScale10 = Color(0xffb91f7d);
+
+static const Color distressScale0 = Color(0xfff1f8fc);
+}
